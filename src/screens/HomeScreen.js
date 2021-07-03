536533b7f0ff2e6327e8d76c1feb349bef6c0e4d
Imran Nazir <63959831+the-cross-art@users.noreply.github.com>
2021-07-03 22:44:44 +0530

    Button added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to Components demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("List")}
+        title="Go to List demo"
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;
